0dc89c1d4930db0bc23375aa9aa09aef183fc47d
Mateus Amorim <142228584+knalen25@users.noreply.github.com>
2023-10-19 10:25:13 -0300

    Create q1_estilizada_inc.c

---
+++ q1_estilizada_inc.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main(){
+    
+    int vetor[10],npos, vezes;
+    
+   for (int i = 0; i < 10; i++)
+   {
+        scanf("%d", &npos);
+        printf("%d", npos);
+        vetor[i] = npos;      
+   }
+   printf("\n");
+        for (vezes= 9 ;vezes >-1; vezes--)
+                
+            {
+                for (int i = 0; i < vezes; i++)
+                    { 
+                        vetor[i] += vetor[i+1];
+        
+                        printf("%d ", vetor[i]);
+                        
+                    }
+
+            }
+
+
+
+return 0;
+}
+    
